e952ac6e20b5914a8864cb43f7a84aa8d145a61d
Katie Pratt <katie.pratt9@gmail.com>
2016-08-22 14:15:11 -0500

    added adjectives and printed them in a for loop

--- FourRamdomAdjective/src/RandomAdjectiveGet.java
+++ FourRamdomAdjective/src/RandomAdjectiveGet.java
@@ -4,10 +4,14 @@ public class RandomAdjectiveGet
 {
 	public static void main(String[] args)
 	{
-		PrintRandom();
-		PrintRandom();
-		PrintRandom();
-		PrintRandom();
+		for(int i =0;i<100;i++)
+		{
+			PrintRandom();
+			PrintRandom();
+			PrintRandom();
+			PrintRandom();
+			System.out.println();
+		}
 	}
 
 	public static void PrintRandom()
@@ -17,107 +21,104 @@ public class RandomAdjectiveGet
 			adj.add("Adamant: unyielding; a very hard substance");
 			adj.add("Adroit: clever, resourceful");
 			adj.add("Antic: clownish, frolicsome");
-					adj.add("Arcadian: serene");
-					adj.add("Baleful: deadly, foreboding");
-					adj.add("Bellicose: quarrelsome (its synonym belligerent can also be a noun)
-			Bilious: unpleasant, peevish
-			Boorish: crude, insensitive
-			Calamitous: disastrous
-			Caustic: corrosive, sarcastic; a corrosive substance
-			Cerulean: sky blue
-			Comely: attractive
-			Concomitant: accompanying
-			Contumacious: rebellious
-			Corpulent: obese
-			Crapulous: immoderate in appetite
-			Defamatory: maliciously misrepresenting
-			Didactic: conveying information or moral instruction
-			Dilatory: causing delay, tardy
-			Dowdy: shabby, old-fashioned; an unkempt woman
-			Efficacious: producing a desired effect
-			Effulgent: brilliantly radiant
-			Egregious: conspicuous, flagrant
-			Endemic: prevalent, native, peculiar to an area
-			Equanimous: even, balanced
-			Execrable: wretched, detestable
-			Fastidious: meticulous, overly delicate
-			Feckless: weak, irresponsible
-			Fecund: prolific, inventive
-			Friable: brittle
-			Fulsome: abundant, overdone, effusive
-			Garrulous: wordy, talkative
-			Guileless: naive
-			Gustatory: having to do with taste or eating
-			Heuristic: learning through trial-and-error or problem solving
-			Histrionic: affected, theatrical
-			Hubristic: proud, excessively self-confident
-			Incendiary: inflammatory, spontaneously combustible, hot
-			Insidious: subtle, seductive, treacherous
-			Insolent: impudent, contemptuous
-			Intransigent: uncompromising
-			Inveterate: habitual, persistent
-			Invidious: resentful, envious, obnoxious
-			Irksome: annoying
-			Jejune: dull, puerile
-			Jocular: jesting, playful
-			Judicious: discreet
-			Lachrymose: tearful
-			Limpid: simple, transparent, serene
-			Loquacious: talkative
-			Luminous: clear, shining
-			Mannered: artificial, stilted
-			Mendacious: deceptive
-			Meretricious: whorish, superficially appealing, pretentious
-			Minatory: menacing
-			Mordant: biting, incisive, pungent
-			Munificent: lavish, generous
-			Nefarious: wicked
-			Noxious: harmful, corrupting
-			Obtuse: blunt, stupid
-			Parsimonious: frugal, restrained
-			Pendulous: suspended, indecisive
-			Pernicious: injurious, deadly
-			Pervasive: widespread
-			Petulant: rude, ill humored
-			Platitudinous: resembling or full of dull or banal comments
-			Precipitate: steep, speedy
-			Propitious: auspicious, advantageous, benevolent
-			Puckish: impish
-			Querulous: cranky, whining
-			Quiescent: inactive, untroublesome
-			Rebarbative: irritating, repellent
-			Recalcitrant: resistant, obstinate
-			Redolent: aromatic, evocative
-			Rhadamanthine: harshly strict
-			Risible: laughable
-			Ruminative: contemplative
-			Sagacious: wise, discerning
-			Salubrious: healthful
-			Sartorial: relating to attire, especially tailored fashions
-			Sclerotic: hardening
-			Serpentine: snake-like, winding, tempting or wily
-			Spasmodic: having to do with or resembling a spasm, excitable, intermittent
-			Strident: harsh, discordant; obtrusively loud
-			Taciturn: closemouthed, reticent
-			Tenacious: persistent, cohesive,
-			Tremulous: nervous, trembling, timid, sensitive
-			Trenchant: sharp, penetrating, distinct
-			Turbulent: restless, tempestuous
-			Turgid: swollen, pompous
-			Ubiquitous: pervasive, widespread
-			Uxorious: inordinately affectionate or compliant with a wife
-			Verdant: green, unripe
-			Voluble: glib, given to speaking
-			Voracious: ravenous, insatiable
-			Wheedling: flattering
-			Withering: devastating
-			Zealous: eager, devoted
-			adj.add("funny: ");
-			adj.add("foggy: ");
-			adj.add("fruitful: ");
-			adj.add("florescent: ");
+			adj.add("Arcadian: serene");
+			adj.add("Baleful: deadly, foreboding");
+			adj.add("Bellicose: quarrelsome");
+			adj.add("Bilious: unpleasant, peevish");
+			adj.add("Boorish: crude, insensitive");
+			adj.add("Calamitous: disastrous");
+			adj.add("Caustic: corrosive, sarcastic");
+			adj.add("Cerulean: sky blue");
+			adj.add("Comely: attractive");
+			adj.add("Concomitant: accompanying");
+			adj.add("Contumacious: rebellious");
+			adj.add("Corpulent: obese");
+			adj.add("Crapulous: immoderate in appetite");
+			adj.add("Defamatory: maliciously misrepresenting");
+			adj.add("Didactic: conveying information or moral instruction");
+			adj.add("Dilatory: causing delay, tardy");
+			adj.add("Dowdy: shabby, old-fashioned; an unkempt woman");
+			adj.add("Efficacious: producing a desired effect");
+			adj.add("Effulgent: brilliantly radiant");
+			adj.add("Egregious: conspicuous, flagrant");
+			adj.add("Endemic: prevalent, native, peculiar to an area");
+			adj.add("Equanimous: even, balanced");
+			adj.add("Execrable: wretched, detestable");
+			adj.add("Fastidious: meticulous, overly delicate");
+			adj.add("Feckless: weak, irresponsible");
+			adj.add("Fecund: prolific, inventive");
+			adj.add("Friable: brittle");
+			adj.add("Fulsome: abundant, overdone, effusive");
+			adj.add("Garrulous: wordy, talkative");
+			adj.add("Guileless: naive");
+			adj.add("Gustatory: having to do with taste or eating");
+			adj.add("Heuristic: learning through trial-and-error or problem solving");
+			adj.add("Histrionic: affected, theatrical");
+			adj.add("Hubristic: proud, excessively self-confident");
+			adj.add("Incendiary: inflammatory, spontaneously combustible, hot");
+			adj.add("Insidious: subtle, seductive, treacherous");
+			adj.add("Insolent: impudent, contemptuous");
+			adj.add("Intransigent: uncompromising");
+			adj.add("Inveterate: habitual, persistent");
+			adj.add("Invidious: resentful, envious, obnoxious");
+			adj.add("Irksome: annoying");
+			adj.add("Jejune: dull, puerile");
+			adj.add("Jocular: jesting, playful");
+			adj.add("Judicious: discreet");
+			adj.add("Lachrymose: tearful");
+			adj.add("Limpid: simple, transparent, serene");
+			adj.add("Loquacious: talkative");
+			adj.add("Luminous: clear, shining");
+			adj.add("Mannered: artificial, stilted");
+			adj.add("Mendacious: deceptive");
+			adj.add("Meretricious: whorish, superficially appealing, pretentious");
+			adj.add("Minatory: menacing");
+			adj.add("Mordant: biting, incisive, pungent");
+			adj.add("Munificent: lavish, generous");
+			adj.add("Nefarious: wicked");
+			adj.add("Noxious: harmful, corrupting");
+			adj.add("Obtuse: blunt, stupid");
+			adj.add("Parsimonious: frugal, restrained");
+			adj.add("Pendulous: suspended, indecisive");
+			adj.add("Pernicious: injurious, deadly");
+			adj.add("Pervasive: widespread");
+			adj.add("Petulant: rude, ill humored");
+			adj.add("Platitudinous: resembling or full of dull or banal comments");
+			adj.add("Precipitate: steep, speedy");
+			adj.add("Propitious: auspicious, advantageous, benevolent");
+			adj.add("Puckish: impish");
+			adj.add("Querulous: cranky, whining");
+			adj.add("Quiescent: inactive, untroublesome");
+			adj.add("Rebarbative: irritating, repellent");
+			adj.add("Recalcitrant: resistant, obstinate");
+			adj.add("Redolent: aromatic, evocative");
+			adj.add("Rhadamanthine: harshly strict");
+			adj.add("Risible: laughable");
+			adj.add("Ruminative: contemplative");
+			adj.add("Sagacious: wise, discerning");
+			adj.add("Salubrious: healthful");
+			adj.add("Sartorial: relating to attire, especially tailored fashions");
+			adj.add("Sclerotic: hardening");
+			adj.add("Serpentine: snake-like, winding, tempting or wily");
+			adj.add("Spasmodic: having to do with or resembling a spasm, excitable, intermittent");
+			adj.add("Strident: harsh, discordant; obtrusively loud");
+			adj.add("Taciturn: closemouthed, reticent");
+			adj.add("Tenacious: persistent, cohesive,");
+			adj.add("Tremulous: nervous, trembling, timid, sensitive");
+			adj.add("Trenchant: sharp, penetrating, distinct");
+			adj.add("Turbulent: restless, tempestuous");
+			adj.add("Turgid: swollen, pompous");
+			adj.add("Ubiquitous: pervasive, widespread");
+			adj.add("Uxorious: inordinately affectionate or compliant with a wife");
+			adj.add("Verdant: green, unripe");
+			adj.add("Voluble: glib, given to speaking");
+			adj.add("Voracious: ravenous, insatiable");
+			adj.add("Wheedling: flattering");
+			adj.add("Withering: devastating");
+			adj.add("Zealous: eager, devoted");
+
 		}
-		int num= (int )(Math. random() * 3);
+		int num= (int )(Math. random() * 98);
 		System.out.println(adj.get(num));
 	}
 }
